e644260b9d1efd67b768dffc5c6e7e0df1b611d8
Nathan Grigg <nathan@nathanamy.org>
2012-04-11 17:15:30 -0700

    Clean up rsync command

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,8 @@
-#/ usr/bin/env bash
+#! /bin/sh
 
 time jekyll
 
 echo
 echo "Sending changes to server"
 echo
-time rsync -rlpgoDiz --delete --checksum --whole-file --filter='protect extended_log' --filter='protect stats/' --chmod=Da+rx,Fa+r _site/ dante.u.washington.edu:public_html/
+time rsync -rczi --delete --whole-file --filter='protect extended_log' --filter='protect stats/' --chmod=Da+rx,Fa+r _site/ dante.u.washington.edu:public_html/
