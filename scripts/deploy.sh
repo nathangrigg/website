f6920f2734fb5a39f2fd9617b8b11412e177734f
Nathan Grigg <nathan@nathanamy.org>
2012-04-17 08:25:05 -0700

    Change host to homer

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,4 +5,4 @@ time jekyll
 echo
 echo "Sending changes to server"
 echo
-time rsync -rczi --delete --whole-file --filter='protect extended_log' --filter='protect stats/' --filter='protect *.pyc' --chmod=Da+rx,Fa+r _site/ dante.u.washington.edu:public_html/
+time rsync -rczi --delete --whole-file --filter='protect extended_log' --filter='protect stats/' --filter='protect *.pyc' --chmod=Da+rx,Fa+r _site/ homer.u.washington.edu:public_html/
